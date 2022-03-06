90c73177973ceeadedcff452e55e3362611b17e0
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-03-06 11:15:12 +0530

    Create Print_10_To_1_UsingWhileLoop.java

---
+++ week1/Print_10_To_1_UsingWhileLoop.java
@@ -0,0 +1,22 @@
+//Write a java  program to print 10 to 1 using while loop
+
+package IncludeHelp;
+
+public class Print_10_To_1_UsingWhileLoop
+{
+	public static void main(String args[])
+	{
+		//loop counter initialisation
+		int i=10;
+
+		//print statement
+		System.out.println("Output is : ");
+
+		//loop to print 10 to 1.
+		while(i>=10)	
+		{
+			System.out.println(i);
+			i--;
+		}
+	}
+}
